e7d4f2181a8cebdeceeb485a77f52bc26edad2ba
Alex Cusimayta <alexgabrielcusimaytalobo@gmail.com>
2025-09-05 02:17:49 -0500

    agregar animacionde logo y preguntas frecuentes

---
+++ js/preguntas.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+    const icon = item.querySelector(".faq-icon");
+
+    question.addEventListener("click", () => {
+      item.classList.toggle("active");
+    });
+  });
+});
